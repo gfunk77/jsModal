5ed0903b5b73f027da4f636f25e82bb9df5bb123
gfunk77 <faraci77@gmail.com>
2022-07-29 22:36:55 -0400

    js show and hide modal

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const openBtn = document.querySelector(".btn");
+const closeBtn = document.querySelector(".close-btn");
+const overlay = document.querySelector(".overlay");
+
+openBtn.addEventListener("click", function () {
+  overlay.classList.add("show-modal");
+});
+
+closeBtn.addEventListener("click", function () {
+  overlay.classList.remove("show-modal");
+});
